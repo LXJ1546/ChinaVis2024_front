1c5a7f08250fcd118d5c33461208258d3f513fd8
LXJ1546 <2991239755@qq.com>
2024-05-07 15:29:59 +0800

    chinavis2024项目搭建

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["eslint:recommended",'plugin:prettier/recommended'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    "prettier/prettier": "off"
+  }
+}
---
+++ src/service/index.js
@@ -0,0 +1,13 @@
+import HYRequest from './request'
+
+const hyRequest = new HYRequest({
+  baseURL: 'http://127.0.0.1:5000',
+  timeout: 5000,
+  interceptors: {
+    requestSuccessFn: (config) => {
+      return config
+    }
+  }
+})
+
+export default hyRequest
