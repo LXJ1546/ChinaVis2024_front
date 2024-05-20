22dedd5407d345d8826ba8c2dcc8e442e2ef0f07
LXJ1546 <2991239755@qq.com>
2024-05-20 15:01:22 +0800

    通信

--- src/layout/content/index.js
+++ src/layout/content/index.js
@@ -7,11 +7,22 @@ import Template from '../../views/template/index'
 import Controller from '../../views/controller'
 import Knowledge from '../../views/knowledge'
 import Scatter from '@/views/cluster'
+import Answer from '../../views/answer'
 const Content = () => {
+  const [mode, setMode] = useState(0) //模式0代表答题模式，1代表时间模式
+  const [month, setMonth] = useState(10) //模式0代表答题模式，1代表时间模式
+  const [classNum, setClassNum] = useState(null) //选中的数据集（所有数据集或某个班级）
   function handleClassNum(classnum) {
     setClassNum(classnum)
   }
-  const [classNum, setClassNum] = useState(null) //选中的数据集（所有数据集或某个班级）
+  // 定义新函数，用于更新模式状态
+  const handleMode = (value) => {
+    setMode(value)
+  }
+  // 定义新函数，用于更新月份状态
+  const handleMonth = (value) => {
+    setMonth(value)
+  }
   return (
     <ContentWrapper>
       <div className="container">
@@ -26,13 +37,13 @@ const Content = () => {
         </div>
         <div className="middle">
           <Card className="card4">
-            <Scatter />
+            <Scatter changeMode={handleMode} changeMonth={handleMonth} />
           </Card>
           <Card className="card5"></Card>
         </div>
         <div className="right">
           <Card className="card6">
-            <Template />
+            <Answer mode={mode} month={month} />
           </Card>
           <Card className="card7">
             <Template />
---
+++ src/views/answer/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const AnswerWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .title {
+    height: 30px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #9b9ca0;
+    margin-bottom: 6px;
+    font-size: 16px;
+    opacity: 0.8;
+  }
+  .view {
+    flex: 1 1 0%;
+  }
+`
