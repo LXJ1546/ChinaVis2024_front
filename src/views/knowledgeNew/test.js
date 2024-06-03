eaa440e02bfa4a2847ae9a2c7a3a8c908f38a8e0
hsiif <1851507245@qq.com>
2024-06-03 20:08:04 +0800

    knowledge修改

--- src/views/knowledgeNew/test.js
+++ src/views/knowledgeNew/test.js
@@ -9,10 +9,21 @@ import * as d3 from 'd3'
 import { getClassKnowledgeInfo } from '../../api/index'
 import { createFromIconfontCN } from '@ant-design/icons'
 import './knowledgeStyle.css'
+import Image0 from '../../pic/0.svg' // 导入SVG图片
 
 const IconFont = createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/c/font_4565164_juvpif6y83m.js'
 })
+
+const title_knowledge = {
+  // Question_6RQj2gF3OeK5AmDvThUV: ['m3D1v', 'b3C9s'],
+  Question_q7OpB2zCMmW9wS8uNt3H: ['r8S3g'],
+  Question_QRm48lXxzdP7Tn1WgNOf: ['y9W5d', 'm3D1v'],
+  Question_pVKXjZn0BkSwYcsa7C31: ['y9W5d', 'm3D1v'],
+  Question_lU2wvHSZq7m43xiVroBc: ['y9W5d', 'k4W1c'],
+  Question_x2Fy7rZ3SwYl9jMQkpOD: ['y9W5d', 's8Y2f'],
+  Question_oCjnFLbIs4Uxwek9rBpu: ['g7R2j', 'm3D1v']
+}
 
 const KnowledgeTree = (props) => {
   const {
@@ -24,7 +35,12 @@ const KnowledgeTree = (props) => {
   const ref = useRef() //用于获取d3绘图使用div的宽高
   const [width, setWidth] = useState(0)
   const [height, setHeight] = useState(0)
-  const [data, setDataState] = useState(undefined)
+  const [allData, setAllDataState] = useState(undefined)
+  const [knowledgeState, setknowledgeState] = useState([]) //只展示当前知识点
+  const [viewState, setViewState] = useState(true) //true表示全局视图，false表示只展示当前知识点
+  const [questionIconState, setquestionIconState] = useState(10) //只展示当前知识点
+  const [questionState, setQuestionState] = useState(false) //true表示点击了某个知识点
+  // const [valueInfo, setValueInfo] = useState(undefined) //保存知识点掌握程度和题目正确率的最大最小值
 
   // const data = {
   //   children: [
@@ -405,7 +421,7 @@ const KnowledgeTree = (props) => {
   useEffect(() => {
     getClassKnowledgeInfo(classNum, 'score').then((res) => {
       // console.log('从后端获取数据', res)
-      setDataState(res.info)
+      setAllDataState(res.info)
       // setValueInfo(res.valueInfo)
     })
   }, [classNum, isChangeWeight])
@@ -414,23 +430,55 @@ const KnowledgeTree = (props) => {
   useLayoutEffect(() => {
     setWidth(ref.current.offsetWidth)
     setHeight(ref.current.offsetHeight)
-    console.log('设置宽度', ref.current.offsetWidth)
+    // console.log('设置高度', ref.current.offsetHeight)
   }, [])
 
   useEffect(() => {
-    if (data != undefined) {
+    //有data时才画图，防止出错
+    if (allData != undefined) {
       const divContainer = document.getElementById('knowledgeChart')
-
-      //
+      //图存在时先移除
       while (divContainer.hasChildNodes()) {
         divContainer.removeChild(divContainer.firstChild)
       }
+      let drawData
+      //所有知识点
+      if (knowledgeState.length == 0) {
+        drawData = allData
+      }
+      //一个知识点
+      else if (knowledgeState.length == 1) {
+        let childrenData
+        // console.log('lemgth', knowledgeState.length)
+        for (let i = 0, len = allData.children.length; i < len; i++) {
+          if (allData.children[i].name == knowledgeState[0]) {
+            childrenData = allData.children[i]
+            break
+          }
+        }
+        drawData = { name: 'q1', children: [childrenData] }
+      } else if (knowledgeState.length == 2) {
+        // console.log('lemgth', knowledgeState.length)
+        let childrenData0
+        let childrenData1
 
-      drawChart()
+        for (let i = 0, len = allData.children.length; i < len; i++) {
+          if (allData.children[i].name == knowledgeState[0]) {
+            childrenData0 = allData.children[i]
+          }
+          if (allData.children[i].name == knowledgeState[1]) {
+            childrenData1 = allData.children[i]
+          }
+        }
+        // console.log('test', [childrenData0, childrenData1])
+        drawData = { name: 'q1', children: [childrenData0, childrenData1] }
+      }
+
+      drawChart(drawData)
     }
-  }, [width, height, data]) // 空数组作为第二个参数，表示仅在组件首次渲染时运行
+  }, [width, height, knowledgeState, viewState, allData]) // 空数组作为第二个参数，表示仅在组件首次渲染时运行
 
-  const drawChart = () => {
+  const drawChart = (data) => {
     const formatTip = d3.format('.4f')
 
     const root = d3
@@ -440,15 +488,33 @@ const KnowledgeTree = (props) => {
 
     const color = d3
       .scaleLinear()
-      .domain([0.3778, 0.5303]) // 输入数据范围
-      .range(['#ffccd5', '#c81d25']) // 输出颜色范围
+      .domain([0.36, 0.52]) // 输入数据范围
+      .range(['#ffccd5', '#E0464E']) // 输出颜色范围
 
     const color1 = d3
       .scaleLinear()
-      .domain([0.2538, 0.7461]) // 输入数据范围
-      .range(['#ffccd5', '#c81d25']) // 输出颜色范围
+      .domain([0.26, 0.81]) // 输入数据范围
+      .range(['#ffccd5', '#E0464E']) // 输出颜色范围
 
-    const dx = 11
+    // 初始化计数器
+    let count_deep3 = 0
+
+    // 遍历节点并计算深度为3的节点总数
+    root.each(function (d) {
+      if (d.depth === 3) {
+        count_deep3++
+      }
+    })
+    // console.log('节点上', count_deep3)
+    // console.log('高度', height)//642
+
+    // const dx = viewState ? 11 : height / count_deep3 / 1.5
+    let dx
+    if (knowledgeState.length == 0) {
+      dx = 11
+    } else {
+      dx = height / count_deep3 / 1.8
+    }
     const dy = width / root.height
 
     // Create a tree layout.
@@ -473,23 +539,38 @@ const KnowledgeTree = (props) => {
       .select('#knowledgeChart')
       .append('svg')
       .attr('width', width)
-      .attr('height', height)
-      .attr('viewBox', [-dy / 3 + 200, x0 - dx - 20, width, height])
+      .attr('height', height + 12)
+      .attr('viewBox', [120, -380, width, height + 12])
       .attr('style', 'max-width: 100%; height: auto; font: 10px sans-serif;')
 
     const rectType = svg.append('g')
     rectType
       .append('text')
       .text('主知识点')
-      .attr('transform', `translate(190, -380)`)
+      .attr('transform', `translate(190, -370)`)
     rectType
       .append('text')
       .text('子知识点')
-      .attr('transform', `translate(400, -380)`)
+      .attr('transform', `translate(405, -370)`)
     rectType
       .append('text')
       .text('题目')
-      .attr('transform', `translate(630, -380)`)
+      .attr('transform', `translate(630, -370)`)
+    rectType
+      .append('svg:image')
+      .attr('xlink:href', function () {
+        return Image0 // 根据索引 i 更新 x 位置，并固定 y 位置
+      })
+      .attr('x', 25)
+      .attr('y', -11)
+      .attr('width', 15)
+      .attr('height', 15)
+      .attr('transform', `translate(650, -370)`)
+
+    rectType
+      .append('text')
+      .text('多知识点题目')
+      .attr('transform', `translate(690, -370)`)
 
     // 删除第一个元素，这里第一个是总的，实际上这里并不需要
     let temp = root.descendants()
@@ -497,14 +578,22 @@ const KnowledgeTree = (props) => {
 
     // 删除第一个元素，这里第一个是总的，实际上这里并不需要
     let templ = root.links()
-    templ.shift()
-    templ.shift()
-    templ.shift()
-    templ.shift()
-    templ.shift()
-    templ.shift()
-    templ.shift()
-    templ.shift()
+    //全局视图时，去除连接到假祖先点的8条线
+    if (knowledgeState.length == 0) {
+      templ.shift()
+      templ.shift()
+      templ.shift()
+      templ.shift()
+      templ.shift()
+      templ.shift()
+      templ.shift()
+      templ.shift()
+    } else if (knowledgeState.length == 1) {
+      templ.shift()
+    } else if (knowledgeState.length == 2) {
+      templ.shift()
+      templ.shift()
+    }
 
     //线条
     svg
@@ -549,9 +638,14 @@ const KnowledgeTree = (props) => {
       let nodes = d.ancestors()
       nodes.reverse().shift() //删除祖先点
       //   console.log('数据', nodes)
-      return `${nodes
-        .map((d) => d.data.name)
-        .join('/')}\n掌握程度：${formatTip(d.data.score)}\n总分值：${d.value}`
+      if (d.depth == 3) {
+        return `${nodes
+          .map((d) => d.data.name)
+          .join('/')}\n正确率：${formatTip(d.data.score)}\n总分值：${d.value}`
+      } else
+        return `${nodes
+          .map((d) => d.data.name)
+          .join('/')}\n掌握程度：${formatTip(d.data.score)}\n总分值：${d.value}`
     })
 
     const circles = node
@@ -560,6 +654,9 @@ const KnowledgeTree = (props) => {
         if (!d.depth) return '#ccc'
         else if (d.depth == 3) return color1(d.data.score)
         else return color(d.data.score)
+      })
+      .attr('class', (d) => {
+        if (d.depth == 3) return 'questionRect'
       })
       .attr('r', 0)
 
@@ -571,23 +668,37 @@ const KnowledgeTree = (props) => {
       .duration(1000)
       // 设置要动画化的属性
       .attr('r', (d) => {
-        return d.value * 1.7
+        if (viewState) {
+          if (d.value >= 3) return d.value * 1.7
+          else return d.value * 2.3
+        } else {
+          if (d.value >= 10) return d.value * 2
+          else if (d.value >= 3) return d.value * 4
+          else if (d.value >= 2) return d.value * 6
+          else return d.value * 8
+        }
       })
 
     // 添加鼠标滑过事件监听器
     circles.on('mouseover', function () {
       // 在回调函数中修改样式
-      // d3.select(this).style('opacity', 1)
+      let radius = +d3.select(this).attr('r')
+      // console.log('滑入', radius)
       d3.select(this)
         .style('stroke', 'RGB(22, 106, 100)')
-        .style('stroke-width', 2)
+        .style('stroke-width', 1)
+        .attr('r', radius + 6)
     })
 
     // 添加鼠标移出事件监听器
     circles.on('mouseout', function () {
       // 还原样式
-      // d3.select(this).style('opacity', 0.9)
-      d3.select(this).style('stroke', 'none')
+      let radius = +d3.select(this).attr('r')
+      // console.log('滑出', radius)
+
+      d3.select(this)
+        .style('stroke', 'none')
+        .attr('r', radius - 6)
     })
 
     node
@@ -610,19 +721,97 @@ const KnowledgeTree = (props) => {
           return d.data.name
         }
       })
+      .attr('class', (d) => {
+        if (d.depth == 3) {
+          if (Object.prototype.hasOwnProperty.call(d.data, 'icon')) {
+            // console.log('icon', d.data.icon)
+            return `addIcon _${d.data.icon}`
+          }
+        }
+      })
       .attr('stroke', 'white')
       .attr('paint-order', 'stroke')
 
-    //题目添加点击事件
-    const myQuestions = d3.selectAll('g.questionRect')
+    //题目添加点击事件,题目圆
+    const myQuestions = d3.selectAll('circle.questionRect')
     myQuestions.on('click', function (e) {
       // 在这里编写点击事件的处理逻辑
       let question = e.target.__data__.data.name
       let Q_name = 'Q_' + question.substring(9, 12)
-      console.log('点击了 <g> 元素', Q_name)
+      // console.log('点击了 <g> 元素', Q_name)
 
       handleHighLightedXaix(Q_name)
       handleClickTitleFlag(1)
+    })
+
+    // 为主知识点添加点击事件
+    const myGroups = d3.selectAll('g.mainKNowledge')
+
+    // 为选中的元素添加点击事件
+    myGroups.on('click', function (e) {
+      // 在这里编写点击事件的处理逻辑
+      let Knowledge = e.target.__data__.data.name
+      setViewState(() => !viewState)
+      if (viewState) {
+        setknowledgeState([Knowledge])
+      } else {
+        setknowledgeState([])
+      }
+      console.log('点击了 <g> 元素', Knowledge)
+    })
+
+    //图标
+    let icons
+
+    //不是问题模式绘制所有图标
+    if (!questionState) {
+      icons = d3.selectAll('g.addIcon')
+    } else {
+      icons = d3.selectAll(`g.addIcon.${questionIconState}`)
+      // console.log('点击图标', `g.addIcon.${questionIconState}`, icons)
+    }
+    // icons = d3.selectAll('g.addIcon')
+
+    // console.log('icon', icons)
+    icons
+      .append('svg:image')
+      .attr('class', function (d) {
+        return `iconImage _${d.data.icon}`
+      })
+      .attr('xlink:href', function () {
+        return Image0 // 根据索引 i 更新 x 位置，并固定 y 位置
+      })
+      .attr('x', 138)
+      .attr('y', function () {
+        return 0
+      })
+      .attr('width', 15)
+      .attr('height', 15)
+      .attr('transform', function () {
+        // 使用 transform 属性来根据数据更新图像位置
+        return 'translate(-90, -8)' // 根据索引 i 更新 x 位置，并固定 y 位置
+      })
+
+    //为iconImage添加点击事件
+    const iconsImage = d3.selectAll('text.addIcon')
+    // 为选中的元素添加点击事件
+    iconsImage.on('click', function (e) {
+      // 获取被点击元素的主知识点
+      let Knowledge = title_knowledge[e.target.__data__.data.name]
+      console.log('点击图标', e.target, Knowledge)
+      let clickedRect = this
+      // 获取第二个类名
+      let secondClassName = clickedRect.classList[1]
+      // 在控制台中打印第二个类名
+      console.log(secondClassName)
+      setquestionIconState(secondClassName)
+      setQuestionState(() => !questionState)
+      //不知道为什么要!
+      if (!questionState) {
+        setknowledgeState(() => Knowledge)
+      } else {
+        setknowledgeState([])
+      }
     })
   }
 
